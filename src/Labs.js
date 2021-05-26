19e33be11fe0cd3fd4408b4a4ae56f9f2bfa574d
Kevin Gislason <kevingislason@gmail.com>
2021-05-26 15:31:31 -0400

    add mint and reclaim to labs

--- src/Labs.js
+++ src/Labs.js
@@ -1,126 +1,54 @@
 import React, { useState, useRef } from 'react';
-import { Form, Grid, Header, Message, Label, Icon } from 'semantic-ui-react';
+import { Form, Grid, Header, Message, Label, Icon, Dropdown } from 'semantic-ui-react';
 import { TxButton } from './substrate-lib/components';
 import { base64Decode, base64Validate, base64Encode } from '@polkadot/util-crypto';
+import PrivateTransfer from './PrivateTransfer'
+import Mint from './Mint'
+import Reclaim from './Reclaim'
 
 export default function Main ({ accountPair }) {
-  const EXPECTED_PAYLOAD_SIZE_IN_BYTES = 608;
-
-  const [transferInfo, setTransferInfo] = useState(null);
-  const [sender1, setSender1] = useState('');
-  const [sender2, setSender2] = useState('');
-  const [receiver1, setReceiver1] = useState('');
-  const [receiver2, setReceiver2] = useState('');
-  const [proof, setProof] = useState('');
-  const [status, setStatus] = useState(null);
-
-  const fileUploadRef = useRef();
-  const [uploadErrorText, setUploadErrorText] = useState('');
-
-  const formatBase64StringForDisplay = base64String => {
-    if (!base64String) {
-      return '';
+    const dropdownItems = [
+    {
+      key: "Private transfer",
+      text: "Private transfer",
+      value: "Private transfer",
+    },
+    {
+      key: "Mint",
+      text: "Mint",
+      value: "Mint",
+    },
+    {
+      key: "Reclaim",
+      text: "Reclaim",
+      value: "Reclaim",
     }
-    const LINE_LENGTH = 75;
-    let formattedString = '';
-    for (let i = 0; i < base64String.length; i++) {
-      formattedString += base64String.slice(i, i + LINE_LENGTH);
-      formattedString += '\n';
-      i += LINE_LENGTH;
-    }
-    return formattedString;
-  };
+  ];
+  const [dropdownState, changeDropdownState] = useState(dropdownItems[0])
 
-  const displayTransferInfo = transferInfoBytes => {
-    setTransferInfo(transferInfoBytes)
-    setSender1(base64Encode(transferInfoBytes.slice(0, 96)));
-    setSender2(base64Encode(transferInfoBytes.slice(97, 192)));
-    setReceiver1(base64Encode(transferInfoBytes.slice(193, 304)));
-    setReceiver2(base64Encode(transferInfoBytes.slice(305, 416)));
-    setProof(base64Encode(transferInfoBytes.slice(317, 608)));
-  };
-
-  const validateFileUpload = fileText => {
-    try {
-      base64Validate(fileText);
-    } catch (error) {
-      fileUploadRef.current.value = null;
-      setUploadErrorText('File is not valid base64');
-      return false;
-    }
-    const bytes = base64Decode(fileText.trim());
-    if (bytes.length !== EXPECTED_PAYLOAD_SIZE_IN_BYTES) {
-      fileUploadRef.current.value = null;
-      setUploadErrorText(`File is ${bytes.length} bytes, expected 608`);
-      return false;
-    }
-    return true;
-  };
-
-  const handleFileUpload = () => {
-    setUploadErrorText('');
-    displayTransferInfo([]);
-    const file = fileUploadRef.current.files[0];
-    if (!file) {
-      return;
-    }
-    const reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = async (readerEvent) => {
-      const text = (readerEvent.target.result);
-      if (validateFileUpload(text)) {
-        displayTransferInfo(base64Decode(text));
-      }
-    };
-  };
+  console.log(dropdownState)
+  let page = <div/>
+  if (dropdownState === 'Mint') {
+    page = <Mint accountPair={accountPair} />
+  } else if (dropdownState === 'Private transfer') {
+    page = <PrivateTransfer accountPair={accountPair} />
+  } else if (dropdownState === 'Reclaim') {
+    page = <Reclaim accountPair={accountPair} />
+  }
 
   return (
-    <Grid.Column width={10}>
-      <Header textAlign='center'>Private Transfer</Header>
-      <Form>
-        <Form.Field inline='true' style={{ textAlign: 'center' }}>
-          <Label basic color='teal'>
-              Upload a private payment file (608 bytes)
-            </Label>
-          <input
-            accept='.txt'
-            id='file'
-            type='file'
-            onChange={handleFileUpload}
-            ref={fileUploadRef}
-            style={{ marginLeft: 0, marginTop: '.5em', border: '0px'}}
-          />
-          <Message
-            error
-            onDismiss={() => setUploadErrorText('')}
-            header='Upload failed'
-            content={uploadErrorText}
-            visible={uploadErrorText.length}
-          />
-        </Form.Field>
-        <Form.Field>
-          <p><b>Sender1:</b> {formatBase64StringForDisplay(sender1)}</p>
-          <p><b>Sender2:</b> {formatBase64StringForDisplay(sender2)}</p>
-          <p><b>Receiver1:</b> {formatBase64StringForDisplay(receiver1)}</p>
-          <p><b>Receiver2:</b> {formatBase64StringForDisplay(receiver2)}</p>
-          <p><b>Proof:</b> {formatBase64StringForDisplay(proof)}</p>
-        </Form.Field>
-        <Form.Field style={{ textAlign: 'center' }}>
-          <TxButton
-            accountPair={accountPair}
-            label='Submit'
-            type='SIGNED-TX'
-            setStatus={setStatus}
-            attrs={{
-              palletRpc: 'mantaPay',
-              callable: 'privateTransfer',
-              inputParams: [transferInfo],
-              paramFields: [true]
-            }}
-          />
-        </Form.Field>
-        <div style={{ overflowWrap: 'break-word' }}>{status}</div>
-      </Form>
-    </Grid.Column>
-  );
-}
+    <div textAlign='left'>
+      <Dropdown
+        onChange={(e, {value}) => {
+          changeDropdownState(value)
+          console.log('value', value)
+        }}
+        placeholder=''
+        selection
+        options={dropdownItems}
+        style={{marginBottom: '2em' }}
+      />
+      {page}
+    </div>
+  )
+};
---
+++ src/Mint.js
@@ -0,0 +1,110 @@
+import React, { useState, useRef } from 'react';
+import { Form, Grid, Header, Message, Label, Icon } from 'semantic-ui-react';
+import { TxButton } from './substrate-lib/components';
+import { base64Decode, base64Validate, base64Encode } from '@polkadot/util-crypto';
+
+export default function Main ({ accountPair }) {
+  const EXPECTED_PAYLOAD_SIZE_IN_BYTES = 112;
+
+  const [mintInfo, setMintInfo] = useState(null);
+  const [status, setStatus] = useState(null);
+
+
+  const fileUploadRef = useRef();
+  const [uploadErrorText, setUploadErrorText] = useState('');
+
+  const formatBase64StringForDisplay = base64String => {
+    if (!base64String) {
+      return '';
+    }
+    const LINE_LENGTH = 75;
+    let formattedString = '';
+    for (let i = 0; i < base64String.length; i++) {
+      formattedString += base64String.slice(i, i + LINE_LENGTH);
+      formattedString += '\n';
+      i += LINE_LENGTH;
+    }
+    return formattedString;
+  };
+
+  const displayMintInfo = transferInfoBytes => {
+    setMintInfo(transferInfoBytes)
+  };
+
+  const validateFileUpload = fileText => {
+    try {
+      base64Validate(fileText);
+    } catch (error) {
+      fileUploadRef.current.value = null;
+      setUploadErrorText('File is not valid base64');
+      return false;
+    }
+    const bytes = base64Decode(fileText);
+    if (bytes.length !== EXPECTED_PAYLOAD_SIZE_IN_BYTES) {
+      fileUploadRef.current.value = null;
+      setUploadErrorText(`File is ${bytes.length} bytes, expected ${EXPECTED_PAYLOAD_SIZE_IN_BYTES}`);
+      return false;
+    }
+    return true;
+  };
+
+  const handleFileUpload = () => {
+    setUploadErrorText('');
+    displayMintInfo([]);
+    const file = fileUploadRef.current.files[0];
+    if (!file) {
+      return;
+    }
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = async (readerEvent) => {
+      const text = (readerEvent.target.result.trim());
+      if (validateFileUpload(text)) {
+        displayMintInfo(base64Decode(text));
+      }
+    };
+  };
+
+  return (
+    <Grid.Column width={10}>
+      <Header textAlign='center'>Mint Private Asset</Header>
+      <Form>
+        <Form.Field inline='true' style={{ textAlign: 'center' }}>
+          <Label basic color='teal'>
+              Upload a mint file (112 bytes)
+            </Label>
+          <input
+            accept='.txt'
+            id='file'
+            type='file'
+            onChange={handleFileUpload}
+            ref={fileUploadRef}
+            style={{ marginLeft: 0, marginTop: '.5em', border: '0px'}}
+          />
+          <Message
+            error
+            onDismiss={() => setUploadErrorText('')}
+            header='Upload failed'
+            content={uploadErrorText}
+            visible={uploadErrorText.length}
+          />
+        </Form.Field>
+        <Form.Field style={{ textAlign: 'center' }}>
+          <TxButton
+            accountPair={accountPair}
+            label='Submit'
+            type='SIGNED-TX'
+            setStatus={setStatus}
+            attrs={{
+              palletRpc: 'mantaPay',
+              callable: 'mintPrivateAsset',
+              inputParams: [mintInfo],
+              paramFields: [true]
+            }}
+          />
+        </Form.Field>
+        <div style={{ overflowWrap: 'break-word' }}>{status}</div>
+      </Form>
+    </Grid.Column>
+  );
+}
