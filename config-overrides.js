090745b75814231cfbb3afefe59fd6f2de3645cd
Kevin Gislason <kevingislason@gmail.com>
2021-05-29 20:30:49 -0400

    wasm scaffolding

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,30 @@
-module.exports = function override(webpackConfig) {
-  webpackConfig.module.rules.push({
+const path = require('path');
+
+module.exports = function override(config, env) {
+  config.module.rules.push({
     test: /\.mjs$/,
     include: /node_modules/,
     type: "javascript/auto"
   });
 
-  return webpackConfig;
-}
+  const wasmExtensionRegExp = /\.wasm$/;
+
+  // make the file loader ignore wasm files
+  let fileLoader = null;
+  config.module.rules.forEach(rule => {
+    (rule.oneOf || []).map(oneOf => {
+      if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
+        fileLoader = oneOf;
+      }
+    });
+  });
+  fileLoader.exclude.push(wasmExtensionRegExp);
+
+  // Add a dedicated loader for them
+  config.module.rules.push({
+    test: wasmExtensionRegExp,
+    include: path.resolve(__dirname, 'src'),
+    use: [{ loader: require.resolve('wasm-loader'), options: {} }],
+  });
+  return config;
+};
