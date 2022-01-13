fdd3affc9eb2d309b75d8706f12e9246619cc837
dansoft <48925763+dansofter@users.noreply.github.com>
2022-01-12 21:23:04 -0500

    feature copy receive address to clipboard
    feature copy receive address to clipboard

--- src/pages/TransactPage/privateAssetTabs/PrivateReceiveTab.js
+++ src/pages/TransactPage/privateAssetTabs/PrivateReceiveTab.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import Button from 'components/elements/Button';
 import Svgs from 'resources/icons';
 import { useSubstrate } from 'contexts/substrateContext';
@@ -9,8 +11,22 @@ import signerInterfaceConfig from 'config/signerInterfaceConfig';
 
 const PrivateReceiveTab = () => {
   const { api } = useSubstrate();
-  const [currentAddress, setCurrentAddress] = useState(null);
   const { selectedAssetType } = useSelectedAssetType();
+  const [currentAddress, setCurrentAddress] = useState(null);
+  const [addressCopied, setAddressCopied] = useState(false);
+
+  const currentAddressString = currentAddress && `${currentAddress.toString()}`;
+
+  useEffect(() => {
+    let interval;
+    if (addressCopied) {
+      interval = setInterval(() => {
+        setAddressCopied(false);
+      }, 2000);
+    }
+
+    return () => clearInterval(interval);
+  }, [addressCopied]);
 
   useEffect(() => {
     setCurrentAddress(null);
@@ -34,7 +50,10 @@ const PrivateReceiveTab = () => {
     }
   };
 
-  const currentAddressString = currentAddress && `${currentAddress.toString()}`;
+  const handleCopyToClipboard = () => {
+    navigator.clipboard.writeText(currentAddressString);
+    setAddressCopied(true);
+  };
 
   return (
     <div className="receive-content">
@@ -45,9 +64,23 @@ const PrivateReceiveTab = () => {
         {currentAddress && (
           <div className="flex pb-4 pt-2 px-2 items-center text-xl">
             <img className="pr-2" src={Svgs.WalletIcon} alt="setting-icon" />
-            <span className="text-lg wrap-text manta-gray">
-              {currentAddressString}
-            </span>
+            {addressCopied ? (
+              <span className="text-lg wrap-text text-primary text-center flex-grow h-28 flex justify-center items-center -ml-5">
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  color="#24A148"
+                  className="mr-1"
+                />
+                Address copied to clipboard
+              </span>
+            ) : (
+              <span
+                className="text-lg wrap-text manta-gray cursor-pointer underline h-28"
+                onClick={handleCopyToClipboard}
+              >
+                {currentAddressString}
+              </span>
+            )}
           </div>
         )}
       </div>
--- src/pages/TransactPage/publicAssetTabs/PublicReceiveTab.js
+++ src/pages/TransactPage/publicAssetTabs/PublicReceiveTab.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import Svgs from 'resources/icons';
 import { useExternalAccount } from 'contexts/externalAccountContext';
 
 const PublicReceiveTab = () => {
   const { externalAccount } = useExternalAccount();
+
+  const [addressCopied, setAddressCopied] = useState(false);
+
+  const handleCopyToClipboard = () => {
+    navigator.clipboard.writeText(externalAccount.address);
+    setAddressCopied(true);
+  };
+
+  useEffect(() => {
+    let interval;
+    if (addressCopied) {
+      interval = setInterval(() => {
+        setAddressCopied(false);
+      }, 2000);
+    }
+
+    return () => clearInterval(interval);
+  }, [addressCopied]);
 
   return (
     <div className="receive-content">
@@ -14,9 +34,23 @@ const PublicReceiveTab = () => {
         {externalAccount && (
           <div className="flex pb-4 pt-2 px-2 items-center text-xl">
             <img className="pr-2" src={Svgs.WalletIcon} alt="setting-icon" />
-            <span className="text-lg wrap-text manta-gray">
-              {externalAccount.address}
-            </span>
+            {addressCopied ? (
+              <span className="text-lg wrap-text text-primary text-center flex-grow h-14 flex justify-center items-center -ml-5">
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  color="#24A148"
+                  className="mr-1"
+                />
+                Address copied to clipboard
+              </span>
+            ) : (
+              <span
+                className="text-lg wrap-text manta-gray cursor-pointer underline h-14"
+                onClick={handleCopyToClipboard}
+              >
+                {externalAccount.address}
+              </span>
+            )}
           </div>
         )}
       </div>
