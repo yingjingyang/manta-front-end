7a4f818b1664d3ac8b68b83134520b3ba46a9fae
Kevin Gislason <kevingislason@gmail.com>
2022-01-28 16:20:19 -0500

    fixed module loading

--- craco.config.js
+++ craco.config.js
@@ -7,6 +7,25 @@ module.exports = {
       ],
     },
   },
+  babel: {
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ]
+  },
+  webpack: {
+    configure: {
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            loader: require.resolve('@open-wc/webpack-import-meta-loader'),
+          }
+        ]
+      }
+    }
+  }
 }
 
 
