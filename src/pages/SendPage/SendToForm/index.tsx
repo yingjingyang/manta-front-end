cdb3eb51187d269f28ff9226311d4a6869f2c1de
dansofter <48925763+dansofter@users.noreply.github.com>
2022-06-14 12:53:58 -0400

    add sync status for e2e testing (#160)

--- src/pages/SendPage/SendToForm/index.tsx
+++ src/pages/SendPage/SendToForm/index.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import PublicPrivateToggle from 'pages/SendPage/PublicPrivateToggle';
 import SendButton from '../SendButton';
 import { useSend } from '../SendContext';
+import { usePrivateWallet } from 'contexts/privateWalletContext';
 import SendToPrivateAddressForm from './SendToPrivateAddressForm';
 import SendToPublicAddressForm from './SendToPublicAddressForm';
 
 const SendToForm = () => {
   const { toggleReceiverIsPrivate, receiverAssetType } = useSend();
+  const { walletIsBusy, isInitialSync } = usePrivateWallet();
 
   return (
     <div className="flex flex-col gap-4">
@@ -24,6 +26,10 @@ const SendToForm = () => {
         )}
       </div>
       <SendButton />
+      <data
+        value={isInitialSync || walletIsBusy.current}
+        data-testing-id="sync-status"
+      />
     </div>
   );
 };
