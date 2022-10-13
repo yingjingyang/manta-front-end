a55184ee42f5b7232c5bcde1925abc7dfdcd2cba
boyuan <dengboyuan2001@gmail.com>
2022-10-13 10:37:48 -0700

    add tailsman icon

--- src/components/Accounts/AccountSelect.tsx
+++ src/components/Accounts/AccountSelect.tsx
@@ -37,7 +37,7 @@ const AccountSelect = () => {
 
   const getAccountIcon = (source: string) => {
     if (source === 'talisman') {
-      return Svgs.TailsmanIcon;
+      return Svgs.Talisman;
     } else if (source === 'polkadot-js') {
       return Svgs.PolkadotJSIcon;
     } else {
