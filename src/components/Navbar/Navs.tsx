78646c93b2cc342bd4f7f1e26d6c10640257d5a3
boyuan <dengboyuan2001@gmail.com>
2022-11-29 17:52:24 -0800

    merge/ handle merge conflict'

--- src/components/Navbar/Navs.tsx
+++ src/components/Navbar/Navs.tsx
@@ -4,28 +4,28 @@ import { useConfig } from 'contexts/configContext';
 
 const Navs = () => {
   return (
-    <div className="flex rounded-full bg-secondary shadow-2xl absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <div className="flex flex-row justify-between rounded-full bg-secondary w-113.5 shadow-2xl items-center text-sm">
       <NavLink
         to="/transact"
-        className="px-6 py-3 font-medium rounded-full text-secondary hover:bg-white"
+        className="py-3 w-1/4 rounded-full text-secondary text-center hover:bg-button-secondary hover:text-white hover:font-bold"
       >
-        Transact
+        zkTransact
       </NavLink>
       <NavLink
         to="/bridge"
-        className="px-6 py-3 font-medium rounded-full text-secondary hover:bg-white"
+        className="py-3 w-1/4 rounded-full text-secondary text-center hover:bg-button-secondary hover:text-white hover:font-bold"
       >
         Bridge
       </NavLink>
       <NavLink
         to="/stake"
-        className="px-6 py-3 font-medium rounded-full text-secondary hover:bg-white"
+        className="py-3 w-1/4 rounded-full text-secondary text-center hover:bg-button-secondary hover:text-white hover:font-bold"
       >
         Stake
       </NavLink>
       <a
         href="https://forum.manta.network/"
-        className="px-6 py-3 font-medium rounded-full text-secondary hover:bg-white"
+        className="py-3 w-1/4 rounded-full text-secondary text-center hover:bg-button-secondary hover:text-white hover:font-bold"
         target="_blank"
         rel="noreferrer"
       >
--- src/pages/SendPage/SendFromForm/index.tsx
+++ src/pages/SendPage/SendFromForm/index.tsx
@@ -3,25 +3,12 @@ import React from 'react';
 import PublicPrivateToggle from 'pages/SendPage/PublicPrivateToggle';
 import { useSend } from '../SendContext';
 import SendAssetSelect from './SendAssetSelect';
-import FormErrorText from 'components/Error/FormErrorText';
 
 const SendFromForm = () => {
   const {
     toggleSenderIsPrivate,
     senderAssetType,
-    // userHasSufficientFunds,
-    // txWouldDepleteSuggestedMinFeeBalance
   } = useSend();
-
-  // let errorText = null;
-  // if (userHasSufficientFunds() === false) {
-  //   errorText = 'Insufficient balance';
-  // }
-
-  // let warningText = null;
-  // if (txWouldDepleteSuggestedMinFeeBalance()) {
-  //   warningText = `You need ${senderAssetType.ticker} to pay fees; consider retaining a small balance.`
-  // }
 
   return (
     <div>
@@ -36,7 +23,6 @@ const SendFromForm = () => {
         </div>
       </div>
       <SendAssetSelect />
-      {/* <FormErrorText errorText={errorText} warningText={warningText} /> */}
     </div>
   );
 };
